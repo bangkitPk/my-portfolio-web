ec3b70a51fc15c18b703b2ddfa59b5d842d1e930
bangkit <bangkitputrakristana@gmail.com>
2023-07-21 01:05:00 +0700

    first commit

---
+++ src/components/Biography.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import foto from '../assets/foto.jpg';
+import './Biography.css';
+
+const Biography = () => {
+  return (
+    <div className='bio-container'>
+      <img src={foto} alt="My Photo" />
+      <div className="description">
+        <h2>About Me</h2>
+        <p>
+            I have an interest in the world of web programming. Since I first learned about coding, I realized how fascinating it is to build web applications that can be accessed by people from all over the world. <br /><br />
+            Every day, I am increasingly motivated to continue learning and deepening my knowledge in web programming. I believe that by honing my hard skills in web technologies such as HTML, CSS, JavaScript, and modern frameworks, I will be able to create amazing and innovative digital experiences.
+        </p>
+      </div>
+      <div className="personal-info">
+        <h3>PERSONAL INFO</h3>
+        <ul>
+            <li>Name: <span>Bangkit Putra Kristana</span></li>
+            <li>Email: <span>bangkitputrakristana@gmail.com</span></li>
+            <li>Instagram: <a href="https://instagram.com/bangkit_kris?igshid=ZDc4ODBmNjlmNQ=="><span>bangkit_kris</span></a></li>
+            <li>GitHub: <a href="https://github.com/bangkitPk">bangkitPk</a></li>
+            <li>Linkedin: <a href="www.linkedin.com/in/bangkit-putra-kristana-207457201">www.linkedin.com/in/bangkit-putra-kristana-207457201</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Biography;
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <h1>
+        Hi, I am Bangkit, currently a 4th-semester student majoring in Information Systems at UPN "Veteran" Jawa Timur.
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
